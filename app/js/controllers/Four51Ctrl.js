539f5de97a7d77244d8629e99b10397e6de11b7f
AlexCramer <alex.cramer@evo-creative.com>
2022-02-28 11:25:13 -0500

    Update Four51Ctrl.js

--- app/js/controllers/Four51Ctrl.js
+++ app/js/controllers/Four51Ctrl.js
@@ -29,8 +29,28 @@ function ($scope, $route, $location, $451, User, Order, Security, OrderConfig, C
                 $scope.user.Culture.DateFormat = XLATService.getCurrentLanguage(user.CultureUI, user.Culture.Name)[2];
 
 	            $scope.user.customKitProducts = {
-	                "CS0001-KIT":["CS0001E","CS0001S"]
-	            };
+	                "C0017-KIT":["C0017","C5055"],
+	                "C0742-KIT":["C0742-Custom","C5023"],
+	                "C1364-KIT":["C1364","C5024"],
+	                "C3496-KIT":["C3496","C5024"],
+	                "C4381-KIT":["C4381","C5022"],
+	                "C4382-KIT":["C4382","C5022"],
+	                "C4483-KIT":["C4483","C5022-W"],
+	                "C4669-KIT":["C4669","C5023"],
+	                "C4431-KIT":["C4431","C5023"],
+	                "C0655-KIT":["C0655","C5023"],
+	                "C5387-KIT":["C5387","C5023"],
+	                "C5041-KIT":["C5041","C5023"],
+	                "C5001-KIT":["C5001","C5001-A"],
+	                "C5053-KIT":["C5053","C5055"],
+	                "C5197-KIT":["C5197","C5055"],
+	                "C5219-KIT":["C5219","C5023"],
+	                "C5120-KIT":["C5120","C5032"],
+	                "C5054-KIT":["C5054","C5055"],
+	                "C5213-1":["C5213","C5214"],
+	                "C5135-KIT":["C5135","C5136"],
+	                "C5428-KIT":["C5022","C5428"],
+                };
 	            
 	            if (!$scope.user.TermsAccepted)
 		            $location.path('conditions');
