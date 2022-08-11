ae6a51fc82cbd80a5e57d87c1bd29e417c6b0232
AlexCramer <alex.cramer@evo-creative.com>
2022-08-11 10:26:14 -0400

    Update app.js
    shipping checkbox

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms','OrderCloud-FixedFooter','OrderCloud-CategoryCollapse','OrderCloud-SpecFormPreview','OrderCloud-ProductLightbox','OrderCloud-CheckoutItemSummary','OrderCloud-CategoryCollapse','OrderCloud-Minicart','OrderCloud-ProductSearchInput','OrderCloud-ProductMatrix']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms','OrderCloud-FixedFooter','OrderCloud-CategoryCollapse','OrderCloud-SpecFormPreview','OrderCloud-ProductLightbox','OrderCloud-CheckoutItemSummary','OrderCloud-CategoryCollapse','OrderCloud-Minicart','OrderCloud-ProductSearchInput','OrderCloud-ProductMatrix','OrderCloud-SameAsShippingCheckbox']);
